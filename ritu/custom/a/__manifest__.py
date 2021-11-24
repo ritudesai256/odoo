cf2a753dc6d36e9118652c45ae8dea95c88476fd
ritudesai256 <94303462+ritudesai256@users.noreply.github.com>
2021-11-24 08:05:26 +0530

    Add files via upload

---
+++ ritu/custom/a/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Real Estate',
+    'category' : 'Sales',
+    'application' : True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_menus.xml',
+        'views/estate_property_views.xml',
+    ],
+}
